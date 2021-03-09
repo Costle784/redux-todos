e5d96d0dd88e261bfa6a5846d9de1368bbc77347
Curtis Ostle <curtisostle@gmail.com>
2021-03-09 13:15:23 -0500

    Add API module for front-end testing

---
+++ client/src/api.js
@@ -0,0 +1,71 @@
+export const API = {
+  todos: [
+    {
+      id: generateId(),
+      name: 'Walk the dog',
+      complete: false,
+    },
+    {
+      id: generateId(),
+      name: 'Wash the car',
+      complete: false,
+    },
+    {
+      id: generateId(),
+      name: 'Go to the gym',
+      complete: true,
+    },
+  ],
+  fetchTodos() {
+    return new Promise((res, rej) => {
+      setTimeout(function () {
+        res(todos);
+      }, 2000);
+    });
+  },
+
+  saveTodo(name) {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        const todo = {
+          id: generateId(),
+          name: name,
+          complete: false,
+        };
+        todos = todos.concat([todo]);
+        fail() ? rej(todo) : res(todo);
+      }, 300);
+    });
+  },
+
+  deleteTodo(id) {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        todos = todos.filter((todo) => todo.id !== id);
+        fail() ? rej() : res(todos);
+      }, 300);
+    });
+  },
+
+  saveTodoToggle(id) {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        todos = todos.map((todo) =>
+          todo.id !== id
+            ? todo
+            : Object.assign({}, todo, { complete: !todo.complete })
+        );
+
+        fail() ? rej() : res(todos);
+      }, 300);
+    });
+  },
+};
+
+function fail() {
+  return Math.floor(Math.random() * (5 - 1)) === 3;
+}
+
+function generateId() {
+  return Math.random().toString(36).substring(2);
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { API } from './api';
 
 const app = document.getElementById('app');
 function App() {
+  console.log(API);
   return <h1>Hello World</h1>;
 }
 
